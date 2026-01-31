f75acfc6a9ee8ac3a879894f496f610be6282be8
김현수 <dy6881@naver.com>
2026-01-31 12:51:10 +0900

    delete js content and add array

--- JsStudy/app.js
+++ JsStudy/app.js
@@ -1,8 +1,5 @@
-let greetingText = "Hi I'm Max!";
 let age = 25;
-alert(greetingText);
-alert(age);
-greetingText = "Hi I am really Max!";
-alert(greetingText);
-age = 19;
-alert(age);
+let userName = 'Max';
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+
+alert(hobbies[0]);
