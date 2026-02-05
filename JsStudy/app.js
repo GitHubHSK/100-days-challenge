c53f946bafc0a9475601b9e32b5aa5a3f253c639
김현수 <dy6881@naver.com>
2026-02-05 17:27:12 +0900

    day26 object and property

--- JsStudy/app.js
+++ JsStudy/app.js
@@ -1,5 +1,11 @@
 let age = 25;
 let userName = 'Max';
 let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = {
+    title: 'Developer',
+    place: 'New York',
+    salary: 5000,
+};
 
 alert(hobbies[0]);
+alert(job.title);
