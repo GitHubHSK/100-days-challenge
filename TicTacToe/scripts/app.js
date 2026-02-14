27a64277a8bdef5842b69d3fee852548c6c17b97
김현수 <dy6881@naver.com>
2026-02-14 12:38:37 +0900

    day35 add app js config js contents and add html contents and add configuration css contents

--- TicTacToe/scripts/app.js
+++ TicTacToe/scripts/app.js
@@ -1,5 +1,21 @@
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: "",
+    symbol: "O",
+  },
+  {
+    name: "",
+    symbol: "X",
+  },
+];
+
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
@@ -11,3 +27,5 @@ editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
