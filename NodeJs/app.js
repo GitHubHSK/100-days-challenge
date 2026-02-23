b9cd004080f5da413d1bec09a6809072a7783647
김현수 <dy6881@naver.com>
2026-02-23 12:12:06 +0900

    save data to file

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,20 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 });
 
 app.get("/", function (req, res) {
-  res.send("<form action='/store-user' method='POST'><label>Your Name</label><input type='text' name='username'><button>Submit</button></form>");
+  res.send(
+    "<form action='/store-user' method='POST'><label>Your Name</label><input type='text' name='username'><button>Submit</button></form>",
+  );
 });
 
 app.post("/store-user", function (req, res) {
   const userName = req.body.username;
-  console.log(userName);
+
+  const filePaht = path.join(__dirname, 'data', 'users.json');
+  const fileData = fs.readFileSync(filePaht);
+  const existingUsers = JSON.parse(fileData);
+
+  existingUsers.push(userName);
+
+  fs.writeFileSync(filePaht, JSON.stringify(existingUsers));
+
   res.send("<h1>Username Stored!</h1>");
 });
 
