3869d5c9106667f855be8fe880bf69d64a1856e2
김현수 <dy6881@naver.com>
2026-03-07 15:00:37 +0900

    add Promise examples

--- JsStudy/async.js
+++ JsStudy/async.js
@@ -1,12 +1,20 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
   let fileData;
 
-  fileData = fs.readFile("data.txt", function (error, fileData) {
-    console.log("FIle parsing done!");
-    console.log(fileData.toString());
-  });
+  //   fs.readFile("data.txt", function (error, fileData) {
+  //     console.log("File parsing done!");
+  //     console.log(fileData.toString());
+  //   });
+
+  fs.readFile("data.txt")
+    .then(function (fileData) {
+      console.log("File parsing done!");
+      console.log(fileData.toString());
+      // return anotherAsyncOperation;
+    })
+    .then(function () {});
 
   console.log("Hi there!");
 }
